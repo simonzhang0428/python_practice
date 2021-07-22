e2d0449effb3e8c14f245d32eeac1eb2e4a397f7
simon <simonzhang0428@gmail.com>
2021-07-22 01:14:45 -0700

    interactive mode

---
+++ string_example.py
@@ -0,0 +1,18 @@
+"""
+This is comment / annotation
+"""
+
+seq = ("H", "e", "l", "l", "o")
+s1 = "-"
+print(s1.join(seq))
+
+s2 = ""
+print(s2.join(seq))
+
+msg = "Simon Zhang"
+print(msg)
+
+# %%
+msg2 = "Hello Simon"
+print(msg2)
+# %%
