0fc3847700a06dcb190cc52aa6eb596fcb906729
simon <simonzhang0428@gmail.com>
2021-07-22 13:30:18 -0700

    f'xxx{xxx}' -> f-string

--- if_else.py
+++ if_else.py
@@ -5,3 +5,36 @@ if weather == "sunny" and (is_holiday or date in ["Sat.", "Sun."]):
     print("Go play outside")
 elif not is_holiday and date not in ["Sat.", "Sun."]:
     print("Go to work")
+
+import sys
+
+# keep the code below
+goods = [
+    {"name": "Computer", "price": 1999},
+    {"name": "Mouse", "price": 10},
+    {"name": "Yachts", "price": 20},
+    {"name": "Airplane", "price": 998}
+]
+
+
+asset = int(sys.argv[1])
+input_list = eval(sys.argv[2])
+
+# write your code here
+for choice in input_list:
+    if choice > 4:
+        print("Please re-enter")
+        continue
+    elif choice == 0:
+        break
+
+    good_name, good_price = goods[choice - 1]['name'], goods[choice - 1]['price']
+    print(f'You want to buy: {good_name}')
+    print(f'It is priced at: {good_price}')
+
+    if good_price > asset:
+        print("The balance is low, so go ahead and top up!")
+        break
+    else:
+        asset -= good_price
+        print("Purchase successful!")
---
+++ loop.py
@@ -0,0 +1,14 @@
+for i in range(4):
+    print(i)
+
+num = 10
+while num >= 5:
+    print(num)
+    num -= 1
+
+list(range(10))
+
+a = [1, 2, 3]
+b = ['Simon', 'Helen', 'Dudu']
+for i in zip(a, b):
+    print(i)
